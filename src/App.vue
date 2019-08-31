de27dc4e76aa3b5b1195b1544eb385ee34000ce4
k.sato <keisuke.sato.gp@gmail.com>
2019-08-31 19:38:06 +0900

    set axios

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="shopList">
+      <div class="area">
+        <ul>
+          <li v-for="(information, index) in informations" :key="index">
+            <p class="shop">{{ information.shop }}</p>
+            <p class="tel">{{ information.tel }}</p>
+            <div class="add">
+              <p class="postal">{{ information.postal }}</p>
+              <p class="address">{{ information.address }}</p>
+            </div>
+          </li>
+        </ul>
+        <pre>{{ $data }}</pre>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  // components: {
+  //   HelloWorld
+  // }
+  data () {
+    return {
+      informations: null
+    }
+  },
+  created() {
+    var vm = this
+    this.$axios
+      .get('./shop.json')
+      .then(function (response){
+        vm.informations = response.data
+      })
   }
 }
 </script>
@@ -21,7 +47,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
